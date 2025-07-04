8eb75775c673a85ccb49e0ce2b99fbfc8371afed
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-07-04 15:46:54 +0200

    Add files

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+﻿# Generates OpenAPI schema from the most recent version of the Telegram API
+FROM ghcr.io/sysbot-org/tgscraper:latest AS tgscraper
+
+RUN php /app/vendor/bin/tgscraper app:export-schema --openapi /out/openapi.json
+
+
+# Generates the TypeScript client from the OpenAPI schema
+FROM node:23-alpine AS node
+
+VOLUME /results
+
+WORKDIR /app
+
+COPY package.json package-lock.json ./
+
+RUN npm install
+
+COPY eslint.config.mjs tsconfig.json .editorconfig .prettierrc ./
+COPY src ./src
+
+COPY --from=tgscraper /out/openapi.json /app/schema/openapi.json
+
+RUN npm start
+
+RUN mkdir /results && cp -r /app/schema/openapi.json /app/output/ /results/
---
+++ src/generator.ts
@@ -0,0 +1,93 @@
+﻿import type { OpenApi } from './types';
+import * as config from './config.json';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import assert from 'node:assert';
+import { Eta } from 'eta';
+import { createClient } from '@hey-api/openapi-ts';
+
+interface EndpointRenderData {
+  name: string;
+  description: string;
+  baseEndpointName: string;
+}
+
+interface ClientRenderData {
+  endpoints: EndpointRenderData[];
+}
+
+const fetchSpecification = async (): Promise<OpenApi> => {
+  console.log('Fetching specification...');
+
+  const openApiRes = await fetch(config.specification_url);
+  const openApi = await openApiRes.json();
+  assert(openApi && typeof openApi === 'object');
+  assert('paths' in openApi);
+
+  return openApi;
+};
+
+const getClientRenderData = (openApi: OpenApi): ClientRenderData => {
+  const endpoints: EndpointRenderData[] = [];
+  for (const [path, methods] of Object.entries(openApi.paths)) {
+    assert(path.startsWith('/'), `Path ${path} should start with a slash`);
+    assert(path.split('/').length === 2, `Path ${path} should not have nested paths`);
+    assert(methods['description'], `Path ${path} should have a description`);
+    assert(methods['post'], `Path ${path} should have a POST method`);
+    assert(Object.keys(methods).length === 2, `Path ${path} should have exactly two methods (desc and POST)`);
+
+    const endpoint: EndpointRenderData = {
+      name: path.slice(1),
+      description: methods.description || '',
+      baseEndpointName: path.charAt(1).toUpperCase() + path.slice(2),
+    };
+    endpoints.push(endpoint);
+  }
+  return { endpoints };
+};
+
+const generateEtaFile = async (fileName: string, data: ClientRenderData | object): Promise<void> => {
+  console.log(`Generating ${fileName}.eta...`);
+
+  const eta = new Eta({ views: path.resolve(__dirname, 'templates') });
+  const content = await eta.renderAsync(`${fileName}.eta`, data);
+  await fs.promises.writeFile(`${config.output_folder}/${fileName}.ts`, content, 'utf-8');
+};
+
+const generateTypesFile = async (openApi: OpenApi, outputPath: string): Promise<void> => {
+  const tempClientFolder = path.resolve(config.temp_folder, '.tmp', 'client');
+  await createClient({
+    input: openApi,
+    output: {
+      path: tempClientFolder,
+      clean: true,
+      format: false,
+      lint: false,
+    },
+    plugins: ['@hey-api/client-fetch'],
+  });
+
+  fs.copyFileSync(path.join(tempClientFolder, 'types.gen.ts'), outputPath);
+};
+
+export const generate = async () => {
+  const openApi = await fetchSpecification();
+
+  const renderData = getClientRenderData(openApi);
+
+  fs.mkdirSync(config.output_folder, { recursive: true });
+
+  await generateEtaFile('core', {});
+  await generateEtaFile('index', {});
+  await generateEtaFile('client', renderData);
+  await generateEtaFile('simple-types', renderData);
+  await generateTypesFile(openApi, `${config.output_folder}/types.ts`);
+};
+
+generate()
+  .then(() => {
+    console.log('Done');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
